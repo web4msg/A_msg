0229702cedc9b301d95369c91628fc9b485b3d8a
web4msg <77393167+web4msg@users.noreply.github.com>
2021-02-20 03:28:55 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,14 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+    x.classList.toggle("change");
+}
+
+function openNav() {
+    document.getElementById("myNav").style.display = "block";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+}
+
+setTimeout(() => { alert('Sender messaged you again which says you to "fcuk off" asap !!') }, 10000)
